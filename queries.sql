7605336601f17b9a41c3084298d75719ffdc945e
Joel Gonzalez <espjoemusic@gmail.comgir config --global user.name JoelGonzalez02>
2020-07-29 18:09:38 -0700

    mvp completed

--- queries.sql
+++ queries.sql
@@ -2,8 +2,33 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT p.ProductName, c.CategoryName 
+FROM Product AS p
+JOIN Category AS c 
+on p.CategoryId = c.id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+SELECT o.id, s.CompanyName 
+FROM [Order] as o
+JOIN Shipper as s
+on o.ShipVia = s.Id
+WHERE o.OrderDate < '2012-08-09'
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT p.productname, o.quantity
+FROM Product as p
+JOIN OrderDetail as o 
+on o.productid = p.Id
+WHERE o.orderid = '10251'
+ORDER BY p.productname
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT o.id AS OrderID, c.CompanyName AS CompanyName, e.LastName AS LastName
+FROM [Order] AS o
+JOIN Customer as c
+on c.id = o.customerid
+JOIN Employee as e
+on e.id = o.employeeid
